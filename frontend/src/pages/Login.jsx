ad17595be5d92240872b1bf72a671ef19a982a7a
anas2 <msalmamujawar1717@gmail.com>
2026-02-16 07:09:03 +0000

    Fixed login API URL space issue

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
         e.preventDefault();
         try {
             const response = await axios.post(
-                `${ import.meta.env.VITE_API_URL } /api/auth/login`,
+                `${ import.meta.env.VITE_API_URL }/api/auth/login`,
                 {
                     email,
                     password,
